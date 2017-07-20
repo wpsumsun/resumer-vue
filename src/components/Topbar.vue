a1ae1a9691955c2f6a63f25abe599d9fe3dc32d2
summer <wpsumsun@126.com>
2017-07-20 14:15:47 +0800

    添加注册对话框

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,48 +3,71 @@
         <div class="wrapper">
             <span class="logo">Resumer</span>
             <div class="actions">
-                <button class="primary">保存</button>
-                <button>预览</button>
+                <a href="#" class="button primary" @click.prevent='signUpDialogVisible=true'>注册</a>
+                <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible=false">
+                    我是slot内容
+                </MyDialog>
+                <a href="#" class="button">登录</a>
+                <button class="button primary">保存</button>
+                <button class="button">预览</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+
+    import MyDialog from './MyDialog'
+
     export default{
-        name:'topbar'
+        name:'topbar',
+        data:function(){
+            return {
+                signUpDialogVisible:false
+            }
+        },
+        components:{
+            MyDialog
+        }
     }
 </script>
 
 <style scoped lang="scss">
+  // 自行查阅 scoped 的功能
+  // 见：https://cn.vuejs.org/v2/guide/comparison.html#CSS-的组件作用域
   #topbar{
     background:#ffffff;
     box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
     >.wrapper{
-        min-width: 1024px;
-        max-width: 1440px;
-        margin: 0 auto;
-        height:64px;
-        padding:0 16px;
-    }    
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height:64px;
+    }
     >.wrapper{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
     }
     .logo{
-        font-size:24px;
-        color:#000000;
+      font-size:24px;
+      color:#000000;
     }
   }
-  button{ 
+  .button{ // 由于加了 scoped， 所以这个 button 选择器只在本组件内有效，不会影响其他组件
     width:72px;
     height:32px;
     border: none;
     cursor: pointer;
-    font-size: 18px; 
+    font-size: 18px; // 设计稿上是 20px，看起来太大，就改成 18px 了
     background:#ddd;
     color: #222;
+    text-decoration: none;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover{
       box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
     }
@@ -52,5 +75,7 @@
       background:#02af5f;
       color: white;
     }
+  }
+  .actions > a{
   }
 </style>
