343fa08618cb9aa9f3072c5f6e741b394f490b2d
summer <wpsumsun@126.com>
2017-07-20 11:51:54 +0800

    使用localStorage存储数据

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,11 @@ export default {
   components:{Topbar,ResumeEditor,ResumerPreview},
   created(){
     document.body.insertAdjacentHTML('afterbegin',icons)
+    let state=localStorage.getItem('state')
+    if(state){
+      state=JSON.parse(state)
+    }
+    this.$store.commit('initState',state)
   }
 }
 </script>
